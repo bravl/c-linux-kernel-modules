cea47c02a0d913b8612a0c10b3fcdeb735ecee73
Bram Vlerick <bv.ext@oip.be>
2018-06-18 15:42:56 +0200

    Update working V4L2 dummy device

--- dummy-v4l2-driver/v4l2-driver.c
+++ dummy-v4l2-driver/v4l2-driver.c
@@ -15,11 +15,14 @@
 #include <media/v4l2-fh.h>
 #include <media/v4l2-event.h>
 #include <media/v4l2-common.h>
+#include <linux/platform_device.h>
 
 MODULE_DESCRIPTION("Dummy v4l2 driver");
 MODULE_AUTHOR("Bram Vlerick");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
+
+#define DRIVER_NAME "v4l2_dummy"
 
 struct dummy_v4l2_device {
         struct v4l2_device v4l2_dev;
@@ -27,16 +30,33 @@ struct dummy_v4l2_device {
         struct mutex mutex;
 };
 
-static struct video_device dd_template = {
-        .name		= "dummy dev",
-        .fops       = NULL,
-        .ioctl_ops 	= NULL,
-        .release	= video_device_release,
-};
-
 struct dummy_v4l2_device *ddev;
 
-static int __init v4l2_dummy_init(void)
+static int dummy_querycap(struct file *file, void *priv,
+				struct v4l2_capability *cap)
+{
+	pr_info("Querying dummy device capabilities\n");
+	
+	return 0;
+} 
+
+static const struct v4l2_file_operations vd_ops = {
+	.owner = THIS_MODULE,
+	.open = v4l2_fh_open,
+};
+
+static const struct v4l2_ioctl_ops vd_ioctl_ops = {
+	.vidioc_querycap = dummy_querycap,
+};
+
+static struct video_device dd_template = {
+        .name		= "dummy dev",
+        .fops       	= &vd_ops,
+        .ioctl_ops 	= &vd_ioctl_ops,
+        .release	= video_device_release_empty,
+};
+
+static int v4l2_dummy_probe(struct platform_device *dev)
 {
         int res = 0;
         pr_info("Running v4l2 dummy driver\n");
@@ -54,17 +74,24 @@ static int __init v4l2_dummy_init(void)
         res = v4l2_device_register(NULL, &ddev->v4l2_dev);
         if (res) goto ddev_free;
 
+	pr_info("V4L2 device registered\n");
+
         ddev->vfd = video_device_alloc();
         if (!ddev->vfd) goto ddev_unreg;
 
+	pr_info("Video device allocated\n");
+	
         *ddev->vfd = dd_template;
         ddev->vfd->v4l2_dev = &ddev->v4l2_dev;
         ddev->vfd->lock = &ddev->mutex;
+	ddev->vfd->device_caps = V4L2_CAP_STREAMING;
 
-        res = video_register_device(ddev->vfd, VFL_TYPE_GRABBER, -1);
+	pr_info("Template setup complete\n");
+
+        video_set_drvdata(ddev->vfd, ddev);
+        
+	res = video_register_device(ddev->vfd, VFL_TYPE_GRABBER, -1);
         if (res < 0) goto ddev_rel;
-
-        //video_set_drvdata(ddev->vfd, ddev);
 
         v4l2_info(&ddev->v4l2_dev, "Registered V4L2 device as %s\n",
                   video_device_node_name(ddev->vfd));
@@ -83,12 +110,61 @@ static int __init v4l2_dummy_init(void)
         return res;
 }
 
-static void __exit v4l2_dummy_exit(void)
+static int v4l2_dummy_remove(struct platform_device *dev)
 {
         pr_info("Removing v4l2 driver\n");
         video_unregister_device(ddev->vfd);
         v4l2_device_unregister(&ddev->v4l2_dev);
         kfree(ddev);
+	return 0;
+}
+
+static struct platform_driver v4l2_dummy_driver = {
+        .probe = v4l2_dummy_probe,
+        .remove = v4l2_dummy_remove,
+        .driver = {
+                .name = DRIVER_NAME,
+        },
+};
+
+static struct platform_device *v4l2_dummy_device;
+
+static int __init v4l2_dummy_init(void)
+{
+        int ret = 0;
+
+        // Register platform driver
+        ret = platform_driver_register(&v4l2_dummy_driver);
+        if (ret < 0) goto end;
+
+        // Allocate platform device (Cleans up memory resources once released)
+        v4l2_dummy_device = platform_device_alloc("v4l2_dummy", 0);
+        if (!v4l2_dummy_device) {
+                ret = -ENOMEM;
+                goto unreg;
+        }
+
+        // Add platform device to device hierarchy
+        ret = platform_device_add(v4l2_dummy_device);
+        if (ret < 0) goto put;
+
+        dev_info(&v4l2_dummy_device->dev, "Dummy V4L2 platform device registered\n");
+
+        return ret;
+
+put:
+        platform_device_put(v4l2_dummy_device);
+unreg:
+        platform_driver_unregister(&v4l2_dummy_driver);
+end:
+        return ret;
+}
+
+static void __exit v4l2_dummy_exit(void)
+{
+        platform_device_unregister(v4l2_dummy_device);
+        platform_driver_unregister(&v4l2_dummy_driver);
+
 }
 
 module_init(v4l2_dummy_init);
